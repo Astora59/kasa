b73868e4a7d37af3c7ca9f81a3df9d033069059e
Yamin Moulay <yaminmoulay12@gmail.com>
2023-10-25 09:34:24 +0200

    changes in ficheLogement

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,10 +3,10 @@ import classes from "./Banner.module.scss"
 
 
 
-export default function Banner(children) {
+export default function Banner(props) {
     return (
         <div className={classes.banner}>
-         <img src={children} alt="banner background" className={classes.banner__image} />
+         <img src={props} alt="banner background" className={classes.banner__image} />
          <p className={classes.banner__text}>Chez vous, partout et ailleurs</p>   
         </div>
     );
